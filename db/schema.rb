bc3a641bcb763bb897b2ec717796e5b644d257f1
pgarnham <pgarnham@uc.cl>
2019-06-15 18:43:54 -0400

    Se pueden agregar imagenes a los posts creados

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190615191530) do
+ActiveRecord::Schema.define(version: 20190615213953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -164,6 +164,7 @@ ActiveRecord::Schema.define(version: 20190615191530) do
     t.string "author"
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image"
   end
 
   create_table "profesors", force: :cascade do |t|
